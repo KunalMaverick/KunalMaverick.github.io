877797929a2019514a0d12de105a97f491d8e255
Kunal K <kunalkishoremaverick@gmail.com>
2024-11-03 21:58:58 +0530

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://KunalMaverick.github.io',
-  base: '/KunalMaverick.github.io',
+  base: '/KunalMaverick.github.io'',
+  outDir: './dist',
+  build: {
+    assets: 'assets'
+  }
 });
